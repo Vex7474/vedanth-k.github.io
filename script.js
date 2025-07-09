363ebc0ebdedc88ff35762510335550092ba0559
Vex7474 <160389451+Vex7474@users.noreply.github.com>
2025-07-09 18:39:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 120,
+      "density": {
+        "enable": true,
+        "value_area": 1000
+      }
+    },
+    "color": {
+      "value": ["#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF"]
+    },
+    "shape": {
+      "type": "circle",
+    },
+    "opacity": {
+      "value": 0.6,
+      "random": true
+    },
+    "size": {
+      "value": 4
+    },
+    "line_linked": {
+      "enable": true
+    },
+    "move": {
+      "enable": true,
+      "speed": 5,
+      "direction": "bottom",
+      "straight": true,
+      "out_mode": "out"
+    }
+  },
+  "interactivity": {
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": false
+      },
+      "resize": true
+    },
+  },
+  "retina_detect": true
+});
